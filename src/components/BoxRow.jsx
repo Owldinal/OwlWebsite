6373201ad49eb9aa1ffd4f8b8fa92d901574c62e
Owldinal <example@gmail.com>
2024-04-15 12:00:17 +0800

    fix

--- src/components/BoxRow.jsx
+++ src/components/BoxRow.jsx
@@ -1,12 +1,18 @@
 import OwlButton from "@components/Button/index.jsx";
+import ELF from "../../public/ELF.png";
+import Magic from "../../public/Magic.png";
 
 export default function (props) {
 
     return (
         <>
             <div className="tableItem flexBetween">
-                <div
-                    style={{width: '136px'}}>{(props.box_type === 1 ? "Elf" : "Magic Fruit") + " # " + props.token_id}</div>
+                <div style={{width: '136px', display: "flex", alignItems: "center"}}>
+                    <div style={{marginRight: "10px", display: "flex", alignItems: "center"}}>
+                        <img src={props.box_type === 1 ? ELF : Magic} alt={""} width={"35px"}/>
+                    </div>
+                    {(props.box_type === 1 ? "Elf" : "Magic Fruit") + " # " + props.token_id}
+                </div>
                 <div
                     style={{width: '132px'}}>{props.earning.toString().split('.')[0]}</div>
                 <div style={{width: '37px'}}>{props.apr.toString().split('.')[0] + "%"}</div>
--- src/components/OwlRow.jsx
+++ src/components/OwlRow.jsx
@@ -1,14 +1,25 @@
 import OwlButton from "@components/Button/index.jsx";
+import ELF from "../../public/ELF.png";
+import Magic from "../../public/Magic.png";
 
 export default function (props) {
 
     return (
         <>
             <div className="tableItem flexBetween">
-                <div style={{width: '40%'}}>{props.token_id}</div>
+                <div style={{width: '40%', display: "flex", alignItems: "center"}}>
+                    <div style={{marginRight: "10px", display: "flex", alignItems: "center"}}>
+                        <img
+                            src={"https://ipfs.io/ipfs/QmZPvN9YwEjLkrczWgRiPFshBxgx8Z4WXwheoErSKjJiGi/Owldinal" + props.token_id + ".png"}
+                            alt={""} width={"35px"}/>
+                    </div>
+                    {"Owldinal" + " # " + props.token_id}
+                </div>
                 <div style={{width: '40%'}}>{props.is_staking === true ? 'Staked' : 'Available'}</div>
-                <OwlButton text={props.is_staking === true ? 'Claim' : 'Stake'} size="small"
-                           func={props.func}/>
+                <div style={{width: "10%"}}>
+                    <OwlButton text={props.is_staking === true ? 'Claim' : 'Stake'} size="small"
+                               func={props.func}/>
+                </div>
             </div>
         </>
     )
